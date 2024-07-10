03e68a9055d446d7e5b4498b9b05210969c96c87
vansh-000 <23165@iiitu.ac.in>
2024-07-10 19:42:21 +0530

    Done basics

---
+++ src/constants/index.js
@@ -0,0 +1,56 @@
+export const DEFAULT_PLACE = {
+  name: "Una",
+  place_id: "una-1253782",
+  adm_area1: "Himachal Pradesh",
+  adm_area2: "Una",
+  country: "India",
+  lat: "31.46493N",
+  lon: "76.26914E",
+  timezone: "Asia/Kolkata",
+  type: "settlement",
+};
+export const MEASUREMENT_SYSTEMS = {
+  AUTO: "auto",
+  METRIC: "metric",
+  UK: "uk",
+  US: "us",
+  CA: "ca",
+};
+export const UNITS = {
+  metric: {
+    temperature: "°C",
+    precipitation: "mm/h",
+    wind_speed: "m/s",
+    visibility: "km",
+    humidity: "%",
+    uv_index: "",
+    cloud_cover: "%",
+  },
+  us: {
+    temperature: "°F",
+    precipitation: "in/h",
+    wind_speed: "mph",
+    visibility: "mi",
+    humidity: "%",
+    uv_index: "",
+    cloud_cover: "%",
+  },
+  uk: {
+    temperature: "°C",
+    precipitation: "mm/h",
+    wind_speed: "mph",
+    visibility: "mi",
+    humidity: "%",
+    uv_index: "",
+    cloud_cover: "%",
+  },
+  ca: {
+    temperature: "°C",
+    precipitation: "mm/h",
+    wind_speed: "km/h",
+    visibility: "km",
+    humidity: "%",
+    uv_index: "",
+    cloud_cover: "%",
+  },
+};
